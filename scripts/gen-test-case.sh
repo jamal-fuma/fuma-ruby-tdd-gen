68764e7d046169882726ab0dee1615676c6ee580
Jamal Natour <Jamal.Natour@fumasoftware.com>
2018-04-26 12:50:20 +0100

    Patch: make gen-test-case include helper methods

--- scripts/gen-test-case.sh
+++ scripts/gen-test-case.sh
@@ -5,7 +5,10 @@ cat <<-EOF
 # encoding: UTF-8
 require_relative '../../test_helper'
 class $klass < Test::Unit::TestCase
+    include TestMethods
+
     def setup
+        fixtures_dir = File.expand_path("../../../fixtures",__FILE__)
     end
     def teardown
     end
