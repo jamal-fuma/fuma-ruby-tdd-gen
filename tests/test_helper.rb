0860c2b8eb0eeb2c84e86e7fbcc1685c670b6b9d
Jamal Natour <Jamal.Natour@fumasoftware.com>
2018-04-26 12:45:05 +0100

    Patch: add helper module to test_helper

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,3 +1,11 @@
 # encoding: UTF-8
 require 'test/unit'
 
+module TestMethods
+    def assert_content_exists_on_disk(fname,content)
+        dirname  = File.dirname(fname)
+        assert File.directory?(dirname), "expected that #{dirname} dir would exist"
+        assert File.exists?(fname), "expected that #{fname} file would exist"
+        assert_equal content.chomp,File.read(fname).chomp, "expected that #{fname} file would have content"
+    end
+end
